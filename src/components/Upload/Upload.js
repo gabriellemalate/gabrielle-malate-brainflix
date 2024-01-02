d05de6febb0a9d7b27eaef77cd613346e0facebf
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-02 11:49:44 -0800

    initiated upload.js

---
+++ src/components/Upload/Upload.js
@@ -0,0 +1,13 @@
+import "./Header.scss"
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    );
+}
+
+export default UploadPage;
