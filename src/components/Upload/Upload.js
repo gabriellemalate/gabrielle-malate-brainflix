c1f56e313d7c0022962c17f6c8b02cf1d35a83d5
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-03 03:23:12 -0800

    upload page desktop styled

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -9,19 +9,24 @@ function UploadPage() {
             <section className="upload">
                 <div className="upload-eq">
                     <h1 className="upload-head">Upload Video</h1>
-                    <article className="upload-thumb">
-                        <h3 className="upload-thumb__head ">VIDEO THUMBNAIL</h3>
-                        <img className="upload-thumb__preview" alt="Preview" src={bike} />
-                    </article>
-                    <article className="upload-title">
-                        <h3 className="upload-title__head">TITLE YOUR VIDEO</h3>
-                        <textarea className="upload-title__box" placeholder="Add a title to your video" />
-                    </article>
+                    <div className="upload-eq__all">
+                        <article className="upload-thumb">
+                            <h3 className="upload-thumb__head ">VIDEO THUMBNAIL</h3>
+                            <img className="upload-thumb__preview" alt="Preview" src={bike} />
+                        </article>
 
-                    <article className="upload-description">
-                        <h3 className="upload-description__head">ADD A VIDEO DESCRIPTION</h3>
-                        <textarea className="upload-description__box" placeholder="Add a description to your video" />
-                    </article>
+                        <div className="upload-eq__boxes">
+                            <article className="upload-title">
+                                <h3 className="upload-title__head">TITLE YOUR VIDEO</h3>
+                                <textarea className="upload-title__box" placeholder="Add a title to your video" />
+                            </article>
+
+                            <article className="upload-description">
+                                <h3 className="upload-description__head">ADD A VIDEO DESCRIPTION</h3>
+                                <textarea className="upload-description__box" placeholder="Add a description to your video" />
+                            </article>
+                        </div>
+                    </div>
 
                     <div className="upload-buttons">
                         <button className="upload-button">
