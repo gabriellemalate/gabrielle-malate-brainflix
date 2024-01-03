1fcc1348359f3973441f7571742db578648f599d
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-03 02:44:55 -0800

    upload page mobile screen built

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -9,20 +9,29 @@ function UploadPage() {
             <section className="upload">
                 <div className="upload-eq">
                     <h1 className="upload-head">Upload Video</h1>
-                    
-                    <h3 className="">VIDEO THUMBNAIL</h3>
-                    <img className="upload__preview" alt="Preview" src={bike} />
-                    <h3>TITLE YOUR VIDEO</h3>
-                    <textarea>Add a title to your video</textarea>
-                    <h3>ADD A VIDEO DESCRIPTION</h3>
-                    <textarea>Add a description to your video</textarea>
-                    <button>
-                        <div>
-                            <img src={publish} alt="Publish" />
-                            PUBLISH
-                        </div>
-                    </button>
-                    <button>
+                    <article className="upload-thumb">
+                        <h3 className="upload-thumb__head ">VIDEO THUMBNAIL</h3>
+                        <img className="upload-thumb__preview" alt="Preview" src={bike} />
+                    </article>
+                    <article className="upload-title">
+                        <h3 className="upload-title__head">TITLE YOUR VIDEO</h3>
+                        <textarea className="upload-title__box" placeholder="Add a title to your video" />
+                    </article>
+
+                    <article className="upload-description">
+                        <h3 className="upload-description__head">ADD A VIDEO DESCRIPTION</h3>
+                        <textarea className="upload-description__box" placeholder="Add a description to your video" />
+                    </article>
+
+                    <div className="upload-buttons">
+                        <button className="upload-button">
+                            <div className="upload-button-eq">
+                                <img className="upload-button-icon" src={publish} alt="Publish" />
+                                PUBLISH
+                            </div>
+                        </button>
+                    </div>
+                    <button className="upload-cancel">
                         CANCEL
                     </button>
                 </div>
